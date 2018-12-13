a9eeae3498fe40bed6dc13ee9df954faea4d2419
Matthew Scheifer <matthew.scheifer@gmail.com>
2018-12-13 00:43:29 -0800

    Fixed a bunch of bugs. Changed how parameters work.

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import itertools
 import story
 
+def has_duplicates(iterable):
+    return len(iterable) > len(set(iterable))
+
 class EstablishedIdea:
     def __init__(self, concept, arguments):
         self.concept = concept
         self.arguments = arguments
         assert len(concept.parameters) == len(arguments) 
         assert None not in arguments
+
+    def __str__(self):
+        return str(self.concept) + "-" + str(self.arguments)
+
+def get_linked_bound_args(narrative_piece, concept, bound_arguments):
+    linked_bound_args = {}
+    for parameter in concept.parameters:
+        for link_set in narrative_piece.linked_parameters:
+            if parameter in link_set:
+                for other_param in link_set:
+                    if other_param in bound_arguments:
+                        existing_val = linked_bound_args.get(parameter)
+                        assert existing_val is None or exisiting_val is bound_args[other_param]
+                        linked_bound_args[parameter] = bound_arguments[other_param]
+
+    return linked_bound_args
+
 
 # is the required concept understood from the given established idea and arguments we have bound
 # from other understood concepts
@@ -30,57 +50,74 @@ def tryBindAndUpdate(narrative_piece, established_ideas, ideas_that_have_lead_to
     bound_arguments = {}
     used_ideas = []
     for concept in narrative_piece.required_concepts:
+        linked_bound_args = get_linked_bound_args(narrative_piece, concept, bound_arguments)
         # lazily find the first idea that this concept is established by
         idea = next(
             (idea for idea in established_ideas if is_established_by(
-                concept, idea, bound_arguments
+                concept, idea, linked_bound_args
             )),
             None
         )
         if idea is None:
             return None # we didn't find an idea to match this concept
         for p_a in zip(concept.parameters, idea.arguments):
-            assert p_a[0] not in bound_arguments or bound_arguments[p_a[0]] is p_a[1]
+            assert p_a[0] not in bound_arguments
             bound_arguments[p_a[0]] = p_a[1]
         used_ideas.append(idea)
 
+    #TODO: if the bound arguments we picked didn't work out, we need to look for other established
+    # ideas that would lead to different bindings
+
+    output_bound_args = {}
+    for param, arg in bound_arguments.items():
+        if param in narrative_piece.input_to_output_params:
+            output_bound_args[narrative_piece.input_to_output_params[param]] = arg
+
     free_parameters = []
     for param in narrative_piece.output_concept.parameters:
-        if not param in bound_arguments:
+        if not param in output_bound_args:
             # A parameter in output but not in inputs, we need to choose a free arg for this but
             # only if the resulting output concept is not already established.
             free_parameters.append(param)
 
-    bound_and_free_args = None
+    output_args = None
 
     all_free_arg_combos_generator = (itertools.product(
-        *(story.free_arguments[param] for param in free_parameters))
+        *(story.free_arguments[param.p_type] for param in free_parameters))
     )
 
-    # TODO: make sure this loop goes at least once if we have 0 free params
+    # This loop goes once if we have 0 free params
     for possible_free_args in all_free_arg_combos_generator:
 
-        bound_and_free_args = bound_arguments.copy()
+        output_args = output_bound_args.copy()
+
         for param, arg in zip(free_parameters, possible_free_args):
-            bound_and_free_args[param] = arg
+            output_args[param] = arg
+
+        if narrative_piece.output_concept.is_exclusive and has_duplicates(output_args.values()):
+            # We don't allow selecting the same arg twice if the concept is exclusive
+            output_args = None
+            continue
 
         # We don't want to establish the same idea twice otherwise the story scene will seem
         # superflous and redundant
         if any(is_established_by(
-                narrative_piece.output_concept, idea, bound_and_free_args) for idea in established_ideas
+                narrative_piece.output_concept, idea, output_args) for idea in established_ideas
             ):
             # we have already established this idea so skip to the next possible free args
-            bound_and_free_args = None
+            output_args = None
         else:
             break # we can establish a new idea with these free args
 
     # All possible free arg sets lead to already established ideas so skip this piece
-    if bound_and_free_args is None:
+    if output_args is None:
+        #TODO: in this case I actually need to try the whole thing again with different params from
+        # different established ideas
         return None
 
     output_arguments = [] 
     for param in narrative_piece.output_concept.parameters:
-        bound_arg = bound_and_free_args[param]
+        bound_arg = output_args[param]
         assert bound_arg is not None
         output_arguments.append(bound_arg)
 
@@ -92,14 +129,17 @@ def tryBindAndUpdate(narrative_piece, established_ideas, ideas_that_have_lead_to
     for idea in used_ideas:
         ideas_that_have_lead_to_something[idea] = True
 
-    return bound_and_free_args
+    return output_bound_args
 
 established_ideas = []
 ideas_that_have_lead_to_something = {}
 
 stillTelling = True
 
-while stillTelling:
+count = 0
+
+while stillTelling and count < 30:
+    count = count + 1
     bound_arguments = None
     narrative_piece = None
     for piece in story.narrative_pieces:
--- story.py
+++ story.py
@@ -1,53 +1,121 @@
 class NarrativePiece:
-    def __init__(self, required_concepts, output_concept, text, is_end=False):
+    def __init__(self, text, required_concepts, output_concept, input_to_output_params, linked_parameters=[], is_end=False):
+        self.text = text
         self.required_concepts = required_concepts
         self.output_concept = output_concept
-        self.text = text
+        self.input_to_output_params = input_to_output_params
+        self.linked_parameters = linked_parameters
+        assert iter(linked_parameters)
+        for link_set in linked_parameters:
+            assert iter(link_set)
         self.is_end = is_end
 
     def __str__(self):
         return self.text
 
+class Parameter:
+    def __init__(self, p_type):
+        self.p_type = p_type
+
 class Concept:
-    def __init__(self, parameters):
-        self.parameters = parameters
-        assert None not in parameters
+    # is exclusive is whether two parameters can be bound to the same thing
+    def __init__(self, parameter_types, debug_name=None, is_exclusive=False):
+        assert None not in parameter_types
+        self.parameters = [Parameter(p_type) for p_type in parameter_types]
+        self.debug_name = debug_name
+        self.is_exclusive = is_exclusive
 
-charParam = '$char1'
-charParam2 = '$char2'
-charParam3 = '$char3'
-charParams = [charParam, charParam2, charParam3]
+    def __str__(self):
+        if self.debug_name is None:
+            return super().__str__()
+        return self.debug_name
 
-characterHasDeadBrother = Concept([charParam])
-characterIsPI = Concept([charParam])
+# Parm types
+charParam = 'char_param_type'
 
-caseOfMissingFather = Concept([])
+characterHasDeadBrother = Concept([charParam], "deadBro")
+characterIsPI = Concept([charParam], "isPI")
 
-hasACase = {param:Concept([param]) for param in charParams}
-hasGuidance = {param:Concept([param]) for param in charParams}
+# 1st parameter is the detective
+caseOfMissingFather = Concept([charParam], "missingFather")
 
-isPerpetrator = {param:Concept([param]) for param in charParams}
+# 1st parameter is the detective, 2nd is mentor
+# The params are exclusive because you can't get advice from yourself.
+hasGuidance = Concept([charParam, charParam], "hasGuidance", is_exclusive=True)
 
-# I could just make subconcepts using parameters, but then could I make pieces that depend on a
-# specific arg for that parameter?
-# Nah, that's weird for union concepts like the resolved as perp
+# 1st parameter is the detective
+inFathersAppartment = Concept([charParam], "inFathersAppartment")
+# 1st parameter is the detective
+foundDeadFather = Concept([charParam], "foundDeadFather")
+# 1st parameter is the detective, 2nd param is the perp
+foundEvidenceOfPerp = Concept([charParam, charParam], "foundEvidenceOfPerp")
+# 1st parameter is the detective, 2nd param is the perp
+isAtHouse = Concept([charParam, charParam], "isAtHouse")
+# 1st parameter is the perp
+runsAway = Concept([charParam], "runsAway")
+# 1st parameter is the perp
+chasing = Concept([charParam], "chasing")
+# 1st parameter is the perp, 2nd param is the detective
+cornered = Concept([charParam, charParam], "cornered")
+# 1st parameter is the detective
+selfDefenseKillPerp = Concept([charParam], "selfDefenseKillPerp")
+# 1st parameter is the detective
+closure = Concept([charParam], "closure")
 
-#TODO: isResolvedAsPerpetrator is isDead or isArrested
-isResolvedAsPerpetrator = {param:Concept([param]) for param in charParams}
+isPerp = Concept([charParam])
+
+isDead = Concept([charParam])
+isArrested = Concept([charParam])
+isArmed = Concept([charParam], "isArmed")
 
 clientHasClosure = Concept([])
 
-narrative_pieces = [
-    NarrativePiece([], characterIsPI, "Introduce PI"),
-    NarrativePiece([], characterHasDeadBrother, "Introduce dead brother"),
-    NarrativePiece([characterIsPI], caseOfMissingFather, "Father missing case"),
-    NarrativePiece([hasACase[charParam]], hasGuidance[charParam],
-        "Character asks their mentor for help"),
-    NarrativePiece([isPerpetrator[charParam], isResolvedAsPerpetrator[charParam]], clientHasClosure,
-        "Client learns who kidnapped/killed father and thanks PI for closure"),
-]
+def one_to_one_piece(text, req, output):
+    input_to_output_params = dict(zip(req.parameters, output.parameters))
+    assert len(input_to_output_params) == 1
+    return NarrativePiece(text, [req], output, input_to_output_params)
+
+def link_firsts(concept1, concept2):
+    return (concept1, concept2)
+
+narrative_pieces = (
+    [
+        NarrativePiece("Introduce PI", [], characterIsPI, {}),
+        NarrativePiece("Introduce dead brother", [], characterHasDeadBrother, {}),
+        one_to_one_piece("Father missing case", characterIsPI, caseOfMissingFather),
+    ] +
+    ([NarrativePiece("Asks old boss for help", [hasACase], hasGuidance, {hasACase.parameters[0]: hasGuidance.parameters[0]})
+        for hasACase in [caseOfMissingFather]
+    ]) +
+    [
+        NarrativePiece("Secretly, the boss did it.", [hasGuidance], isPerp, {hasGuidance.parameters[1]: isPerp.parameters[0]}),
+        NarrativePiece("Breaks into father's appartment.", [hasGuidance], inFathersAppartment, {hasGuidance.parameters[0]: inFathersAppartment.parameters[0]}),
+        one_to_one_piece("Finds father dead.", inFathersAppartment, foundDeadFather),
+
+        NarrativePiece("Knows now that his old boss did it.", [foundDeadFather, hasGuidance],
+            foundEvidenceOfPerp, dict(zip(hasGuidance.parameters, foundEvidenceOfPerp.parameters)),
+            [link_firsts(foundDeadFather, hasGuidance)]),
+
+        NarrativePiece("Goes to his old bosses place.", [foundEvidenceOfPerp], isAtHouse,
+            dict(zip(foundEvidenceOfPerp.parameters, isAtHouse.parameters))),
+
+        NarrativePiece("Boss sees PI and bolts.", [isAtHouse, isPerp], runsAway,
+            {isPerp.parameters[0]: runsAway.parameters[0]}, [(isAtHouse.parameters[1], isPerp.parameters[0])]),
+
+        one_to_one_piece("PI chases old boss.", runsAway, chasing),
+
+        NarrativePiece("Old boss gets cornered down an alley.", [chasing], cornered, {chasing.parameters[0]: cornered.parameters[0]}),
+        NarrativePiece("Old boss pulls a gun. Fires on our hero and misses. PI kills old boss.",
+            [cornered, isArmed], isDead, {isArmed.parameters[0]: isDead.parameters[0]},
+            [link_firsts(cornered, isArmed)])
+    ] +
+    ([NarrativePiece("Client learns who kidnapped/killed father and thanks PI for closure",
+            [isPerp, perpResolved], clientHasClosure, {}, [link_firsts(isPerp, perpResolved)], is_end=True)
+        for perpResolved in [isDead, isArrested]
+    ])
+)
 
 free_characters = frozenset(["Alan", "Sarah", "David", "Julie"])
 
 # TODO: add other non-character params to this map with their own argument sets
-free_arguments = {charParam:free_characters for charParam in charParams}
+free_arguments = {charParam:free_characters}
