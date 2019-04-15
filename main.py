add5ad9f10aadf98229eeeb98ef91b3677c55ca7
Matthew Scheifer <matthew.scheifer@gmail.com>
2019-04-14 17:14:27 -0700

    Break up looking for valid beats. Start testing.
    More fixes for multiple output concepts.

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ def get_linked_bound_args(narrative_piece, concept, bound_arguments):
 
     return linked_bound_args
 
-
 # is the required concept understood from the given established idea and arguments we have bound
 # from other understood concepts
 def is_established_by(concept, established_idea, bound_arguments):
@@ -42,12 +41,7 @@ def is_established_by(concept, established_idea, bound_arguments):
                        # another concept in this piece
     return True
 
-def tryBindAndUpdate(narrative_piece, established_ideas, ideas_that_have_lead_to_something):
-    if narrative_piece.output_concept is story.story_end:
-        for idea in established_ideas:
-            if not ideas_that_have_lead_to_something.get(idea, False):
-                # We can't end the story yet because not all ideas have lead to something
-                return None
+def can_beat_be_used(narrative_piece, established_ideas):
     bound_arguments = {}
     used_ideas = []
     for concept in narrative_piece.required_concepts:
@@ -74,14 +68,16 @@ def tryBindAndUpdate(narrative_piece, established_ideas, ideas_that_have_lead_to
     output_bound_args = {}
     for param, arg in bound_arguments.items():
         if param in narrative_piece.input_to_output_params:
-            output_bound_args[narrative_piece.input_to_output_params[param]] = arg
+            for out_param in narrative_piece.input_to_output_params[param]:
+                output_bound_args[out_param] = arg
 
     free_parameters = []
-    for param in narrative_piece.output_concept.parameters:
-        if not param in output_bound_args:
-            # A parameter in output but not in inputs, we need to choose a free arg for this but
-            # only if the resulting output concept is not already established.
-            free_parameters.append(param)
+    for output_concept in narrative_piece.output_concepts:
+        for param in output_concept.parameters:
+            if not param in output_bound_args:
+                # A parameter in output but not in inputs, we need to choose a free arg for this but
+                # only if the resulting output concept is not already established.
+                free_parameters.append(param)
 
     output_args = None
 
@@ -89,7 +85,7 @@ def tryBindAndUpdate(narrative_piece, established_ideas, ideas_that_have_lead_to
         *(noir.free_arguments[param.p_type] for param in free_parameters))
     )
 
-    # This loop goes once if we have 0 free params
+    # This loop goes once if we have 0 free params (with an empty set of args)
     for possible_free_args in all_free_arg_combos_generator:
 
         output_args = output_bound_args.copy()
@@ -97,15 +93,21 @@ def tryBindAndUpdate(narrative_piece, established_ideas, ideas_that_have_lead_to
         for param, arg in zip(free_parameters, possible_free_args):
             output_args[param] = arg
 
-        if narrative_piece.output_concept.is_exclusive and has_duplicates(output_args.values()):
+        found_dup = False
+
+        for output_concept in narrative_piece.output_concepts:
+            if output_concept.is_exclusive and has_duplicates(output_args.values()):
+                found_dup = True
+
+        if found_dup:
             # We don't allow selecting the same arg twice if the concept is exclusive
             output_args = None
             continue
 
         # We don't want to establish the same idea twice otherwise the story scene will seem
         # superflous and redundant
-        if any(is_established_by(
-                narrative_piece.output_concept, idea, output_args) for idea in established_ideas
+        if any(is_established_by(output_concept, idea, output_args) for idea in established_ideas
+            for output_concept in narrative_piece.output_concepts
             ):
             # we have already established this idea so skip to the next possible free args
             output_args = None
@@ -118,21 +120,42 @@ def tryBindAndUpdate(narrative_piece, established_ideas, ideas_that_have_lead_to
         # different established ideas
         return None
 
-    output_arguments = [] 
-    for param in narrative_piece.output_concept.parameters:
-        bound_arg = output_args[param]
-        assert bound_arg is not None
-        output_arguments.append(bound_arg)
+    established_ideas = []
 
-    established_idea = EstablishedIdea(narrative_piece.output_concept, output_arguments)
+    for output_concept in narrative_piece.output_concepts:
+        output_arguments = [] 
+        for param in output_concept.parameters:
+            bound_arg = output_args[param]
+            assert bound_arg is not None
+            output_arguments.append(bound_arg)
+
+        established_ideas.append(EstablishedIdea(output_concept, output_arguments))
+
+    return established_ideas, used_ideas
+
+def try_update_with_beat(narrative_piece, established_ideas, ideas_that_have_lead_to_something):
+    for output_concept in narrative_piece.output_concepts:
+        if output_concept is story.story_end:
+            for idea in established_ideas:
+                if not ideas_that_have_lead_to_something.get(idea, False):
+                    # We can't end the story yet because not all ideas have lead to something
+                    return None
+
+    can = can_beat_be_used(narrative_piece, established_ideas)
+
+    if can is None:
+        return None
+
+    ideas_beat_establishes, used_ideas = can
 
     # Now do the update steps becuase our match was succesful
-    established_ideas.append(established_idea)
+
+    established_ideas.extend(ideas_beat_establishes)
 
     for idea in used_ideas:
         ideas_that_have_lead_to_something[idea] = True
 
-    return output_arguments
+    return [arg for arg in (idea.arguments for idea in ideas_beat_establishes)]
 
 if __name__ == '__main__':
     established_ideas = []
@@ -147,7 +170,9 @@ if __name__ == '__main__':
         bound_arguments = None
         narrative_piece = None
         for piece in noir.narrative_pieces:
-            arguments = tryBindAndUpdate(piece, established_ideas, ideas_that_have_lead_to_something)
+            arguments = try_update_with_beat(
+                piece, established_ideas, ideas_that_have_lead_to_something
+            )
             if arguments is not None:
                 narrative_piece = piece
                 break
@@ -156,5 +181,6 @@ if __name__ == '__main__':
             stillTelling = False
         else:
             print(narrative_piece, " - ", arguments)
-            if narrative_piece.output_concept is story.story_end:
-                stillTelling = False
+            for output_concept in narrative_piece.output_concepts:
+                if output_concept is story.story_end:
+                    stillTelling = False
--- test_main.py
+++ test_main.py
@@ -16,7 +16,9 @@ class TestMain(unittest.TestCase):
         concept2 = story.Concept([])
         idea = main.EstablishedIdea(concept, [])
 
+        # Idea of concept establishes it
         self.assertTrue(main.is_established_by(concept, idea, {}))
+        # Idea of different concept does not
         self.assertFalse(main.is_established_by(concept2, idea, {}))
 
         argument = 'woo'
@@ -24,13 +26,29 @@ class TestMain(unittest.TestCase):
         parameterized_concept = story.Concept(['blah'])
         idea_with_arg = main.EstablishedIdea(parameterized_concept, [argument])
 
+        # In a context where the parameter is free, idea of concept with a parameter establishes
+        # the concept (and the parameter should be bound afterwards)
         self.assertTrue(main.is_established_by(parameterized_concept, idea_with_arg, {}))
+        # In a context where the parameter is bound, idea of concept with the same parameter
+        # establishes the concept.
         self.assertTrue(main.is_established_by(parameterized_concept, idea_with_arg, {
             parameterized_concept.parameters[0]:argument,
         }))
+        # In a context where the parameter is bound, idea of concept with a different parameter
+        # does not establish the concept.
         self.assertFalse(main.is_established_by(parameterized_concept, idea_with_arg, {
             parameterized_concept.parameters[0]:'yoyoyo',
         }))
+
+    def test_can_beat_be_used(self):
+        concept = story.Concept([])
+        beat = story.MakeBeat('a').sets_up(concept)
+
+        # A story beat with no requirements can be used even with no pre-established ideas.
+        self.assertTrue(main.can_beat_be_used(beat, []))
+
+        # TODO: test an exclusive and non-exclusive output concept together, that is definitely
+        # broken
 
 if __name__ == '__main__':
     unittest.main()
